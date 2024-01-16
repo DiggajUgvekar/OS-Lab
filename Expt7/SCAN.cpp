7f94231c1b1090fd58ac5a62d8f808f217e1192b
Diggaj Ugvekar <97679616+DiggajUgvekar@users.noreply.github.com>
2024-01-16 20:15:14 +0530

    Update SCAN.cpp

--- Expt7/SCAN.cpp
+++ Expt7/SCAN.cpp
@@ -6,10 +6,11 @@ int scan(vector<int> &rq, int current_head) {
     int head = current_head, distance;
     vector<int> seq;
     sort(rq.begin(), rq.end());
-    auto it = lower_bound(rq.begin(), rq.end(), head);
-    int mid = *it;
+    auto it = lower_bound(rq.begin(), rq.end(), head);        //element greater than head
     int midPos;
     int var=1;
+
+    // we find a number that is less than head or equal to in  the vector
     while(var==1){
          if (head > *it) {
         midPos = it - rq.begin();
@@ -38,6 +39,7 @@ int scan(vector<int> &rq, int current_head) {
         current_head = rq[i];
         seq.push_back(rq[i]); 
     }
+
     cout << "Seek sequence is: " << head << "-->";
     for (int i = 0; i < seq.size(); i++) {
         
